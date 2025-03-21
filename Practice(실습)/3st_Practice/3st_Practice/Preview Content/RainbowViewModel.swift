35a1274a6032c54f8fcb60fb08fe9d7710a4ca79
AppleDeveloper <ikejhc159@gmail.com>
2025-03-21 10:44:52 +0900

    ✨ 3주차 실습 파일
    3주차 실습 파일

---
+++ Practice(실습)/3st_Practice/3st_Practice/Preview Content/RainbowViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RainbowViewModel.swift
+//  3st_Practice
+//
+//  Created by Apple Coding machine on 3/21/25.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class RainbowViewModel {
+    var selectedRainbowModel: RainbowModel?
+    var appleLogoColor: Color?
+}
---
+++ Practice(실습)/3st_Practice/3st_Practice/_st_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_PracticeApp.swift
+//  3st_Practice
+//
+//  Created by Apple Coding machine on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RainbowView()
+        }
+    }
+}
