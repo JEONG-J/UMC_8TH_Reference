d39e62822ad275f7738eedd3a9b86d964b3094fe
AppleDeveloper <ikejhc159@gmail.com>
2025-04-10 11:46:09 +0900

    ✨ 5주차 실습 작성
    5주차 실습 코드 등록

---
+++ Practice(실습)/5st_Practice/5st_Practice/MyModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyModel.swift
+//  5st_Practice
+//
+//  Created by Apple Coding machine on 4/10/25.
+//
+
+import Foundation
+
+struct MyModel: Codable {
+    let nickname: String
+    let hobby: String
+    let favoriteCharacter: String
+    let location: String
+}
---
+++ Practice(실습)/5st_Practice/5st_Practice/_st_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_PracticeApp.swift
+//  5st_Practice
+//
+//  Created by Apple Coding machine on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            JSONParsingView()
+        }
+    }
+}
